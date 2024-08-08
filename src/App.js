c35911040813c07f39fe7609176e67d583705846
Mariahnikee <144451851+Mariahnikee@users.noreply.github.com>
2024-08-08 10:24:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import passport from './passport.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <div> <h1>Welcome to react snippet</h1>
+   </div>
+          <header className="App-header">
+         <img src={passport} className='maria-passport' alt='passport-photo'/>
+      </header>
+      <footer><b>Hello world</b></footer>
+    </div>
+  );
+}
+
+export default App;
